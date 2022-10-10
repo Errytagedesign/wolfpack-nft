95bb1098a6806fbfd38699fc94092d22f325704d
Errytagedesign <errytage@gmail.com>
2022-10-10 18:05:59 +0100

    It's game on

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// components
+
+// style
+import "./App.css";
+import "aos/dist/aos.css";
 
 function App() {
+  const AOS = require("aos");
+  useEffect(() => {
+    AOS.init();
+  }, [AOS]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {" "}
+      hello wolfpack
+      <header></header>
+      <main>
+        <Router>
+          <Routes>
+            <Route />
+          </Routes>
+        </Router>
+      </main>
+      <footer></footer>
     </div>
   );
 }
